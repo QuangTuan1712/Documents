6452ba8714ad65183cba103eff0d66bf0eace091
QuangTuan1712 <76410244+QuangTuan1712@users.noreply.github.com>
2021-01-15 00:55:20 +0300

    Update Main.java

--- Lab 3 - Programming/Main.java
+++ Lab 3 - Programming/Main.java
@@ -1,24 +1,25 @@
 
 public class Main {
 	public static void main(String[] args) {
-		Neznayka nez = new Neznayka("Nezznayka", Locations.STREET);
-		Kozlik koz = new Kozlik("Kozlik", Locations.STREET);
-		Sign sign = new Sign("sign", Locations.DOOR, "Sale of various goods");
-		nez.seeASign(sign);
-		koz.seeASign(sign);
-		nez.location(Locations.STORE);
-		koz.location(Locations.STORE);
-		Items guns = new Items("guns of various systems, calibers");
-		guns.location(Locations.STAND);
-		Items weapons = new Items("Various pistols, knives, finks, daggers, brushes and brass knuckles");
-		weapons.location(Locations.COUNTER);
-		Showcase show_1 = new Showcase("Showcases", Locations.WALL);
-		show_1.description();
-		Items items = new Items("sets of thieves' master keys, steel files, drills, pliers, nippers, crowbars, crowbars for breaking locks, autogenous devices for cutting fireproof cabinets and chests");
-		items.location(Locations.SHOWCASES);
-		Showcase show_2 = new Showcase("Showcase", "Police Utensils");
-		show_2.description();
-		Items policeItems = new Items("rubber electric batons of various styles, steel handcuffs, shackles, incendiary and tear bombs and other police items");
-		policeItems.location(Locations.SHOWCASE);
+		Neznayka nez = new Neznayka("Neznayka", 17);
+		Kozlik koz = new Kozlik("Kozlik", 18);
+		Sign sign = new Sign("Sale of various goods");
+		Door door = new Door(sign);
+		nez.seeASign(door);
+		koz.seeASign(door);
+		Store store = new Store();
+		nez.enterTheStore(store);
+		koz.enterTheStore(store);
+		store.descrip();
+		Stands stands = new Stands(Items.ON_STANDS);
+		stands.showProducts();
+		Counter counter = new Counter(Items.ON_COUNTER);
+		counter.showProducts();
+		Showcase case_1 = new Showcase(Items.ON_SHOWCASE_1);
+		case_1.descrip();
+		case_1.showProducts();
+		Showcase case_2 = new Showcase(Items.ON_SHOWCASE_2, "Police Utensils");
+		case_2.descrip();
+		case_2.showProducts();
 	}
 }
