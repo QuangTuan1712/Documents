42f2d256b4fbe60661fbe28d083e26e9e70cce2a
QuangTuan1712 <76410244+QuangTuan1712@users.noreply.github.com>
2021-05-24 23:35:27 +0300

    Add files via upload

---
+++ Programming/Lab5/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"t1u7a1n2","c":"Main","l":"Main()","url":"%3Cinit%3E()"},{"p":"t1u7a1n2","c":"Main","l":"main(String[])","url":"main(java.lang.String[])"}]
---
+++ Programming/Lab5/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"t1u7a1n2","l":"Main"}]
